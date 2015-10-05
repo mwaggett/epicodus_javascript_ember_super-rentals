dbc51faba3c0a44f5f551555226356324edf1d8e
mwaggett <mwaggett@alumni.reed.edu>
2015-10-04 20:31:30 -0700

    Implement one-to-many relationship between cities and rentals. Things will be weird if you try to update a rental's city..

--- app/routes/city.js
+++ app/routes/city.js
@@ -5,6 +5,15 @@ export default Ember.Route.extend({
     return this.store.findRecord('city', params.city_id);
   },
   actions: {
+    saveRental(params) {
+      var newRental = this.store.createRecord('rental', params);
+      var city = params.city;
+      city.get('rentals').addObject(newRental);
+      newRental.save().then(function() {
+        return city.save();
+      });
+      this.transitionTo('city', params.city);
+    },
     updateCity(city, params) {
       Object.keys(params).forEach(function(key) {
         if(params[key]!==undefined) {
